07d6ac96486540c4e1f47fc75c742e277395da0c
VishSeran <vishwa.sovis@gmail.com>
2025-09-19 11:03:21 +0530

    coding server.js

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import notificationRoutes from './routes/notificationRoutes';
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/notifications', notificationRoutes);
+
+const PORT = process.env.PORT ||5000;
+app.listen(PORT, ()=> console.log(`Notification service running on port: ${PORT}`));
+
