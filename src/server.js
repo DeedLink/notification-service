34b8f816cf8b61057078bb94cb1528fe7d845d40
VishSeran <vishwa.sovis@gmail.com>
2025-10-04 18:58:45 +0530

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import notificationRoutes from './routes/notificationRoutes.js';
 import verificationRoutes from'./routes/verification.js';
+import deedNotificationRoutes from './routes/deedNotification.js';
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use('/api/notifications', notificationRoutes);
 
 app.use('/api/verfication',verificationRoutes);
+
+app.use('/api/verfication',deedNotificationRoutes);
 
 const PORT = process.env.PORT ||5000;
 app.listen(PORT, ()=> console.log(`Notification service running on port: ${PORT}`));
